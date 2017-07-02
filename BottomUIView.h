dbccde73945b406336afddf3a3449d6f3f7ec56c
andforce <86118@163.com>
2017-07-03 00:17:56 +0800

    refactor layout

---
+++ BottomUIView.h
@@ -0,0 +1,13 @@
+//
+//  BottomUIView.h
+//  iBeeboPro
+//
+//  Created by 迪远 王 on 2017/7/2.
+//  Copyright © 2017年 andforce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BottomUIView : UIView
+
+@end
---
+++ BottomUIView.m
@@ -0,0 +1,21 @@
+//
+//  BottomUIView.m
+//  iBeeboPro
+//
+//  Created by 迪远 王 on 2017/7/2.
+//  Copyright © 2017年 andforce. All rights reserved.
+//
+
+#import "BottomUIView.h"
+
+@implementation BottomUIView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
